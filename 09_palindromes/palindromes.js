0435abbe17fd87e230774f8fddc0cfa9afae1f35
Ashutush Choudhury <63922550+Kryoshi@users.noreply.github.com>
2024-04-21 03:50:55 +0530

    Complete 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toUpperCase();
+    let array = string.split('');
+    array = array.filter((char) => (/[a-zA-Z\d]/).test(char));
+    string = array.join('');
+    array.reverse();
+    let reversed = array.join('');
+    return (string === reversed);
 };
 
 // Do not edit below this line
