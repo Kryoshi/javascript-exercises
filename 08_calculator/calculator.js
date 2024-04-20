2bf99217829f5be58ae8e135645a8fb9c1de739c
Ashutush Choudhury <63922550+Kryoshi@users.noreply.github.com>
2024-04-21 03:19:46 +0530

    Complete 01 and 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
+const add = function(a, b) {
+	return a + b;
+};
+
+const subtract = function(a, b) {
+	return a - b;
+};
+
+const sum = function(array) {
+	return array.reduce((sum, number) => sum + number, 0);
+};
+
+const multiply = function(array) {
+  return array.reduce((product, number) => product * number, 1);
+};
+
+const power = function(a, b) {
+  return a ** b;
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(i) {
+  let factorial = 1;
+  for (i; i > 0; --i) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
