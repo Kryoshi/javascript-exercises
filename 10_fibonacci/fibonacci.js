fd2f90aaae6c013bc12518b2fe586b325be97ed5
Ashutush Choudhury <63922550+Kryoshi@users.noreply.github.com>
2024-04-21 03:59:27 +0530

    Complete 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n >= 0) {
+        let last = 0;
+        let next = 1;
+        for (let i = 0; i < +n; ++i) {
+            let temp = next;
+            next += last;
+            last = temp;
+        }
+        return last;
+    }
+    return "OOPS";
 };
 
 // Do not edit below this line
