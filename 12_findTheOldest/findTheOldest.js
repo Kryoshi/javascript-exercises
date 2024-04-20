bb2ccc84d5eb798f05f3dd828d16ac4bda3e12db
Ashutush Choudhury <63922550+Kryoshi@users.noreply.github.com>
2024-04-21 04:24:38 +0530

    Complete 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => (age(oldest) > age(person)? oldest : person));
 };
+
+function age(person) {
+    if (person.yearOfDeath) return person.yearOfDeath - person.yearOfBirth;
+    else return (new Date().getFullYear()) - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
